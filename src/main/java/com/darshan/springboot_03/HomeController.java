8275d765d0cf7e6798d0b20b5075ccf9a4e79ce3
GBTC440009UR <darshan1111@yahoo.com>
2018-07-09 21:04:39 -0400

    Initial commit

---
+++ src/main/java/com/darshan/springboot_03/HomeController.java
@@ -0,0 +1,14 @@
+package com.darshan.springboot_03;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage(){
+        return "hometemplate";
+    }
+}
+
